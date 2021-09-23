42bdc3fe1c5455afddd8c322a70f883d8dc4099b
DiegoAvmor <diegoavmor@gmail.com>
2021-09-23 12:37:10 -0500

    ✅ Added students endpoint test

---
+++ tests/Unit/StudentTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Http\Response;
+
+class StudentTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_get_all_students()
+    {
+        $this->json('get', 'api/v1/students')
+         ->assertStatus(Response::HTTP_OK)
+         ->assertJsonStructure(
+             [
+                 'status',
+                 'message',
+                 'data' => [
+                     '*' =>[
+                         'name',
+                         'semester',
+                         'age'
+                     ]
+                 ]
+             ]
+         );
+    }
+}
